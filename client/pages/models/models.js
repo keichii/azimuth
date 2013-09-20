84535bf4da1785552666b6f91b26b130cd21be60
Lauro Cruz <keichee@gmail.com>
2013-09-20 19:10:28 +0000

    fixed js for models

--- client/pages/models/models.js
+++ client/pages/models/models.js
@@ -1,8 +1,8 @@
 // Accompanying JS file for the page template.
 // Describes the page's metadata and actions.
 
-Template.home.label = 'model';
+Template.home.label = 'models';
 Template.home.description = 'Basic one column layout';
 
 // This important method hooks the template into the CMS
-registry.pageTemplate({name: 'home', label: 'home'})
+registry.pageTemplate({name: 'models', label: 'models'})
