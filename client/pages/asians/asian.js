43243fcd8db73b929f10a0aafe933e6b70f44fbd
Virgil Guevarra <vguevarra@gmail.com>
2013-09-20 18:38:39 +0000

    Asian Pages created

---
+++ client/pages/asians/asian.js
@@ -0,0 +1,8 @@
+// Accompanying JS file for the page template.
+// Describes the page's metadata and actions.
+
+Template.asian.label = 'Asian';
+Template.asian.description = 'Asian';
+
+// This important method hooks the template into the CMS
+registry.pageTemplate({name: 'asian', label: 'Asian'})
