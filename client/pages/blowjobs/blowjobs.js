542b44d3f652f9d0cd1a8db7b2446ccded131185
Virgil Guevarra <vguevarra@gmail.com>
2013-09-20 20:35:33 +0000

    hentai and blowjobs page created

---
+++ client/pages/blowjobs/blowjobs.js
@@ -0,0 +1,8 @@
+// Accompanying JS file for the page template.
+// Describes the page's metadata and actions.
+
+Template.blowjobs.label = 'Blow Jobs';
+Template.blowjobs.description = 'Blow Jobs';
+
+// This important method hooks the template into the CMS
+registry.pageTemplate({name: 'blowjobs', label: 'Blow Jobs'})
