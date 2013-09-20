c6dea48c3ee4215de73d37e83d8da4c368f05434
Virgil Guevarra <vguevarra@gmail.com>
2013-09-20 20:55:34 +0000

    lesbian, milf and orgy pages created

---
+++ client/pages/lesbians/lesbian.js
@@ -0,0 +1,8 @@
+// Accompanying JS file for the page template.
+// Describes the page's metadata and actions.
+
+Template.lesbian.label = 'Lesbian';
+Template.lesbian.description = 'Lesbian';
+
+// This important method hooks the template into the CMS
+registry.pageTemplate({name: 'lesbian', label: 'Lesbian'})
---
+++ client/pages/orgy/orgy.js
@@ -0,0 +1,8 @@
+// Accompanying JS file for the page template.
+// Describes the page's metadata and actions.
+
+Template.orgy.label = 'Orgy';
+Template.orgy.description = 'Orgy';
+
+// This important method hooks the template into the CMS
+registry.pageTemplate({name: 'orgy', label: 'Orgy'})
