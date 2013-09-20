5ef7cc8c994ce5a8166e689bd7535569676a5eab
Roman Crisologo <romanmydesenyo@gmail.com>
2013-09-20 19:57:12 +0000

    models

--- client/pages/models/models.js
+++ client/pages/models/models.js
@@ -1,8 +1,8 @@
 // Accompanying JS file for the page template.
 // Describes the page's metadata and actions.
 
-Template.home.label = 'models';
-Template.home.description = 'Basic one column layout';
+Template.models.label = 'model';
+Template.models.description = 'Basic one column layout';
 
 // This important method hooks the template into the CMS
 registry.pageTemplate({name: 'models', label: 'models'})
--- client/pages/models/models_edit.js
+++ client/pages/models/models_edit.js
@@ -1,7 +1,7 @@
 // Accompanying JS file for the page edit template.
 // Describes the page's metadata and actions.
 
-Template.home_edit.events = {
+Template.models_edit.events = {
   'submit #pageEditForm': events.savePage,
   'click #deletePage': events.showDeletePageModal,
   'click .delete-page': events.deletePage
