7016c3a007214c85d205b01c33ab565ba7d0a3fc
Roman Crisologo <romanmydesenyo@gmail.com>
2013-09-20 18:28:46 +0000

    content for model

--- client/pages/models/models.js
+++ client/pages/models/models.js
@@ -1,7 +1,7 @@
 // Accompanying JS file for the page template.
 // Describes the page's metadata and actions.
 
-Template.home.label = 'home';
+Template.home.label = 'model';
 Template.home.description = 'Basic one column layout';
 
 // This important method hooks the template into the CMS
